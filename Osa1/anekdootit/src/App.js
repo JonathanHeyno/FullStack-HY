9d8bc499c69ded04796bec9683c3b04417252d75
Jonathan Heyno <jona.heyno@gmail.com>
2022-06-30 19:09:27 +0300

    Osa1

---
+++ Osa1/anekdootit/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+
+  const [votes, setVote] = useState(new Uint8Array(anecdotes.length))
+  const [selected, setSelected] = useState(Math.floor(Math.random() * anecdotes.length))
+  const [most, setMostVotes] = useState(selected)
+
+  const handleNextAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVote(copy)
+    if (votes[selected] >= votes[most]) {
+      setMostVotes(selected)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={() => handleVote()} text="vote" />
+      <Button handleClick={() => handleNextAnecdote()} text="next anecdote" />
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[most]}</p>
+      <p>has {votes[most]} votes</p>
+    </div>
+  )
+}
+
+export default App
---
+++ Osa1/unicafe/src/App.js
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+  </tr>
+  )
+}
+
+const Statistics = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value ={good} />
+        <StatisticLine text="neutral" value ={neutral} />
+        <StatisticLine text="bad" value ={bad} />
+        <StatisticLine text="all" value ={good + neutral + bad} />
+        <StatisticLine text="average" value ={(good - bad) / (good + neutral + bad)} />
+        <StatisticLine text="positive" value ={100*(good / (good + neutral + bad)) + " %"} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+
+
+  return (
+    <div>
+      <h3>give feedback</h3>
+      <Button handleClick={() => handleGoodClick()} text="good" />
+      <Button handleClick={() => handleNeutralClick()} text="neutral" />
+      <Button handleClick={() => handleBadClick()} text="bad" />
+      <h3>statistics</h3>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      
+    </div>
+  )
+}
+
+export default App
